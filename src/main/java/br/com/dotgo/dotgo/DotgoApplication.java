cfcfef793e8fe644c1ccf4e5921e66218f19391c
JoaoVignoli <vignolijoao@gmail.com>
2025-07-04 20:22:30 -0300

    Iniciando o repositório com estrutura base para o software.

---
+++ src/main/java/br/com/dotgo/dotgo/DotgoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dotgo.dotgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dotgo/dotgo/DotgoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dotgo.dotgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
