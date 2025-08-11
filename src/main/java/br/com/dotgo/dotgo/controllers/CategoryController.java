30b38cb8bd1bbf9a7170fc232e00cd4e5d96c10b
JoaoVignoli <vignolijoao@gmail.com>
2025-08-11 17:44:33 -0300

    Ajustando Controller, Dtos e Repository da entidade Category.

--- src/main/java/br/com/dotgo/dotgo/controllers/CategoryController.java
+++ src/main/java/br/com/dotgo/dotgo/controllers/CategoryController.java
@@ -3,8 +3,33 @@ package br.com.dotgo.dotgo.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.dotgo.dotgo.dtos.CategoryResponseDto;
+import br.com.dotgo.dotgo.repositories.CategoryRepository;
+
+import java.util.Locale.Category;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponseDto> createNewCategory(@RequestBody String entity) {
+        
+        Category categorySave = new Category();
+        
+
+        return entity;
+    }
     
 }
--- src/main/java/br/com/dotgo/dotgo/repositories/CategoryRepository.java
+++ src/main/java/br/com/dotgo/dotgo/repositories/CategoryRepository.java
@@ -1,5 +1,9 @@
 package br.com.dotgo.dotgo.repositories;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.dotgo.dotgo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
