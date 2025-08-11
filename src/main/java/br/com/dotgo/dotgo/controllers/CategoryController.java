64b568ad7113d0c790202a45d544b5525ca5eac7
JoaoVignoli <vignolijoao@gmail.com>
2025-08-11 17:02:06 -0300

    Adicionando Controller e Repository da entidade Category.

---
+++ src/main/java/br/com/dotgo/dotgo/controllers/CategoryController.java
@@ -0,0 +1,10 @@
+package br.com.dotgo.dotgo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    
+}
