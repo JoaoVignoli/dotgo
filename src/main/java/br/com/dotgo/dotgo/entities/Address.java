29871d17004910ae3ec524e6f291b166d9f8149d
Moucalou <moucalou@gmail.com>
2025-08-14 21:33:08 -0300

    Criado atributos, getter e setters na classe de Endereço, será necessário ajustar o banco de dados na parte do usuário para poder vincular o endereço ao usuário e vice-versa

--- src/main/java/br/com/dotgo/dotgo/entities/Address.java
+++ src/main/java/br/com/dotgo/dotgo/entities/Address.java
@@ -1,5 +1,103 @@
 package br.com.dotgo.dotgo.entities;
 
-public class Address {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn
+    @JsonIgnore
+    private User user;
+    private String street;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private Integer cep;
+    private Integer adress_number;
+    private String observation;
+    private String complement;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+
+    public Integer getAdress_number() {
+        return adress_number;
+    }
+
+    public void setAdress_number(Integer adress_number) {
+        this.adress_number = adress_number;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
 }
