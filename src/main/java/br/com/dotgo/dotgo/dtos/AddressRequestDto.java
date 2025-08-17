e4130617620155d2acdcb340b7ea74502de7b862
Moucalou <moucalou@gmail.com>
2025-08-17 11:01:13 -0300

    Criado atributos dentro do DTO de request do endereço

--- src/main/java/br/com/dotgo/dotgo/dtos/AddressRequestDto.java
+++ src/main/java/br/com/dotgo/dotgo/dtos/AddressRequestDto.java
@@ -1,4 +1,47 @@
 package br.com.dotgo.dotgo.dtos;
 
+import br.com.dotgo.dotgo.entities.User;
+
 public class AddressRequestDto {
+
+    private String street;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private Integer cep;
+    private Integer address_number;
+    private String complement;
+    private Integer idUsuario;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public Integer getAddress_number() {
+        return address_number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
 }
