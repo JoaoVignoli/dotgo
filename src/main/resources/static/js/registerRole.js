fd0316327505306828a24138902dc9d532a2c301
Moucalou <moucalou@gmail.com>
2025-08-19 20:57:08 -0300

    Colocado Ids em cada um dos botões do registerRole e feito o JavaScript funcional para guardar o valor selecionado e redirecionar pra próxima pagina

---
+++ src/main/resources/static/js/registerRole.js
@@ -0,0 +1,30 @@
+function nextStep() {
+    window.location.href = "https://dotgo.vignoli.dev.br//personalInfoRegister";
+}
+
+function registerClient() {
+    localStorage.setItem("userRole", "CLIENT");
+    nextStep();
+}
+
+function registerServiceHolder() {
+    localStorage.setItem("userRole", "SERVICE_HOLDER");
+    nextStep();
+}
+
+function returnWindow() {
+    history.back();
+}
+
+function main() {
+    const returnButton = document.getElementById("returnButton");
+    returnButton.addEventListener("click", returnWindow);
+
+    const clientRoleButton = document.getElementById("clientRoleButton");
+    clientRoleButton.addEventListener("click", registerClient)
+
+    const serviceHolderRoleButton = document.getElementById("serviceHolderRoleButton");
+    serviceHolderRoleButton.addEventListener("click", registerServiceHolder);
+}
+
+window.addEventListener("load", main);
