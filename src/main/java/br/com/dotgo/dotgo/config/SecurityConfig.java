c42269126934b6a0656ab734466fd6681f121ae2
Moucalou <moucalou@gmail.com>
2025-08-20 21:13:25 -0300

    Adicionado as versões HTML de cada página como públicas também

--- src/main/java/br/com/dotgo/dotgo/config/SecurityConfig.java
+++ src/main/java/br/com/dotgo/dotgo/config/SecurityConfig.java
@@ -29,8 +29,9 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/login", "/users/register", "/home", "/login", "/mainFeed.html", "/css/**", "/js/**", "/registerRole", "/personalInfoRegister", "/addressRegister", "/perfilPhoto",
-                "/newProduct", "/productCategories", "/productSubcategories", "/home").permitAll()  
+                .requestMatchers("/auth/login", "/users/register", "/home", "/login", "/mainFeed.html", "/css/**", "/js/**", "/registerRole", "/registerRole.html", "/personalInfoRegister", "/personalInfoRegister.html", "/addressRegister", "/addressRegister.html", 
+                "/perfilPhoto", "/perfilPhoto.html", "/newProduct", "/newProduct.html", "/productCategories", "/productCategories.html",
+                 "/productSubcategories", "/productSubcategories.html").permitAll()  
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
