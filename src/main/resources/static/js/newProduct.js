9ec493958ba57244e08a038e556ee41e9b5c6a47
JoaoVignoli <vignolijoao@gmail.com>
2025-08-21 20:52:14 -0300

    Ajustando novamente visão do modal.

--- src/main/resources/static/js/newProduct.js
+++ src/main/resources/static/js/newProduct.js
@@ -1,6 +1,6 @@
 function closeModal() {
     const modal = document.getElementById("add-product-modal");
-    modal.classList.remove("modal-overlay")
+    modal.classList.remove("modal-overlay");
 
     localStorage.removeItem("userId");
     localStorage.removeItem("userSelectedSubcategoryId")
@@ -9,7 +9,7 @@ function closeModal() {
 
 function showModal() {
     const modal = document.getElementById("add-product-modal");
-    modal.classList.add("hidden")
+    modal.classList.add("modal-overlay");
 }
 
 function returnWindow() {
