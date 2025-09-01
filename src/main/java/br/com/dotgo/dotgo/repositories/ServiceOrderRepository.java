b459a97ebcdf7fe37444d333b17f2e156ed4b52b
JoaoVignoli <vignolijoao@gmail.com>
2025-09-01 20:09:18 -0300

    Criando query para buscar Ordens de Serviço de um prestador especifico.

--- src/main/java/br/com/dotgo/dotgo/repositories/ServiceOrderRepository.java
+++ src/main/java/br/com/dotgo/dotgo/repositories/ServiceOrderRepository.java
@@ -1,9 +1,13 @@
 package br.com.dotgo.dotgo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.dotgo.dotgo.entities.ServiceOrder;
 
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Integer>{
-
+    @Query("SELECT so FROM ServiceOrder so JOIN so.product p WHERE p.user.id = :providerId")
+    List<ServiceOrder> findAllByServiceProviderId(Integer providerId);
 }
